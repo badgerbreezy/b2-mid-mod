5f514ac118f4d23aa235294a5674b2222466a744
Brian Liu <brian.b.liu@gmail.com>
2020-10-23 10:34:52 -0600

    Add test to parks show page to show all rides

--- spec/features/parks/show_spec.rb
+++ spec/features/parks/show_spec.rb
@@ -8,12 +8,34 @@ RSpec.describe 'As a visitor' do
         name: 'Seis Banderas',
         admission: '49.95'
       )
+      @ride_1 = @park_1.rides.create!(
+        name: 'Corredor de Rayos',
+        rating: '9'
+      )
+      @ride_2 = @park_1.rides.create!(
+        name: 'Corredor de Tormenta',
+        rating: '6'
+      )
+      @ride_3 = @park_1.rides.create!(
+        name: 'El Grande Osito',
+        rating: '8'
+      )
+
     end
 # |---------->|tests|<----------|
     it 'I can see the info for that amusement park' do
       visit "/parks/#{@park_1.id}"
       expect(page).to have_content(@park_1.name)
       expect(page).to have_content(@park_1.admission)
+    end
+
+    it 'I can see each rides name and thrill rating for that park' do
+      visit "/parks/#{@park_1.id}"
+
+      within "#ride-#{@ride_2.id}" do
+        expect(page).to have_content(@ride_2.name)
+        expect(page).to have_content(@ride_2.rating)
+      end
     end
   end
 end
